3d6f3764a1b6756603d0d4eab6a38eebce331523
Sagar Parmar <sagarparmar881@gmail.com>
2024-02-16 10:09:06 +0530

    Code changes.

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -5,26 +5,16 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "In , %s!";
 	private final AtomicLong counter = new AtomicLong();
 	
-	WebClient webClient = WebClient.create(); 
 
-
-	@GetMapping("/greeting")
+	@GetMapping("/ingreeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		
-		WebClient.ResponseSpec responseSpec = webClient.get()
-			    .uri("http://localhost:8082/ingreeting")
-			    .retrieve();
-		
-		String responseBody = responseSpec.bodyToMono(String.class).block();
-		
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
